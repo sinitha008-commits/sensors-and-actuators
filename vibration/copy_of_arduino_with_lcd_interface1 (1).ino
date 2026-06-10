0e675c3a124642b2406ff994cd7733c296097418
sinitha008-commits <sinitha008@gmail.com>
2026-06-10 15:42:46 +0530

    Add files via upload

---
+++ vibration/copy_of_arduino_with_lcd_interface1 (1).ino
@@ -0,0 +1,20 @@
+/*
+
+https://www.tinkercad.com/things/fuatKiLaSpG
+
+*/
+
+int motorPin = 3; //motor transistor is connected to pin 3
+
+void setup()
+{
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(motorPin, HIGH); //vibrate
+  delay(1000);  // delay one second
+  digitalWrite(motorPin, LOW);  //stop vibrating
+  delay(1000); //wait 50 seconds.
+}
